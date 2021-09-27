b53c4d7b653871f6525eade547b3af454b28fe1a
yutake27 <takei.y.ah@m.titech.ac.jp>
2021-09-27 22:28:38 +0900

    feat: use GraphQL by post request

--- src/data/target_selection/fetch_resolution.py
+++ src/data/target_selection/fetch_resolution.py
@@ -9,6 +9,15 @@ class GraphQL:
         res = requests.get(cls.api_url + query)
         return res.json()
 
+    @classmethod
+    def post(cls, query: str, variables: list):
+        res = requests.post(cls.api_url, json={'query': query, 'variables': variables})
+        print(res.status_code)
+        print(res.headers)
+        print(res.text)
+        assert res.status_code == 200
+        return res.json()
+
 
 def main():
     entry_list = ["4HHB", "12CA", "3PQR"]
@@ -19,6 +28,11 @@ def main():
     print(res_json)
     resolution_list = [ent['rcsb_entry_info']['resolution_combined'] for ent in res_json['data']['entries']]
     print(resolution_list)
+
+    query = 'query($ids: [String!]!){entries(entry_ids: $ids){rcsb_entry_info{resolution_combined}}}'
+    variables = {'ids': entry_list}
+    res_json = GraphQL.post(query, variables)
+    print(res_json)
 
 
 if __name__ == '__main__':
