37808f51f914c8e4f1d7844df9e0938ccd4b913c
yutake27 <takei.y.ah@m.titech.ac.jp>
2021-10-18 15:11:05 +0900

    feat: copy Uniclust30 into SSD (in tsubame)

--- src/alphafold/alphafold.sh
+++ src/alphafold/alphafold.sh
@@ -6,11 +6,26 @@
 #$ -j y
 # The execution time is specified when submitting a job according to the sequence length.
 
+
+date
 # Init (For TSUBAME)
 . /etc/profile.d/modules.sh
 module purge
 module load cuda/11.0.3 alphafold/2.0.0
 module li
+
+# Copy uniclust30 database to SSD (only on tsubame)
+if [ -z "$TMPDIR" ]; then
+    echo Copy database to '$TMPDIR' ($TMPDIR)
+    echo =================================
+    echo `date` cp uniclust30 database
+    cp $ALPHAFOLD_DATA_DIR/uniclust30/uniclust30_2018_08_hhsuite.tar.gz $TMPDIR
+    echo `date` finish copy
+    pushd $TMPDIR
+    echo `date` compress uniclust30_2018_08_hhsuite.tar.gz
+    tar xvf uniclust30_2018_08_hhsuite.tar.gz --use-compress-prog=pigz
+    popd
+fi
 
 # Set Alphafold execution file directory
 # alphafold_path="SET YOUR OWN PATH"
