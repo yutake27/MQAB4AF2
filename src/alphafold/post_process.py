03cca326eef9597c88ca8d293abc5456e2c131c5
yutake27 <takei.y.ah@m.titech.ac.jp>
2021-11-18 21:55:28 +0900

    feat: save number of missing and different residues

--- src/alphafold/post_process.py
+++ src/alphafold/post_process.py
@@ -32,26 +32,35 @@ def main():
     print('Downloading native pdb...')
     pdb_id, chain = target_name.split('_')
     print(f'PDB ID: {pdb_id}, Chain: {chain}')
-    DownloadProtein.download_pdb_specific_chain(pdb_id, chain, str(fasta_path), str(native_pdb_path))
+    if not native_pdb_path.exists():
+        DownloadProtein.download_pdb_specific_chain(pdb_id, chain, str(fasta_path), str(native_pdb_path))
+    num_diff, num_missing, length = DownloadProtein.test_match_pdb_and_fasta(str(native_pdb_path), str(fasta_path))
     assert native_pdb_path.exists()
 
     # Get model accuracy
     print('Get model accuracy...')
-    print('Running TMscore')
-    tmscore_df = ModelAccuracy.get_gdt_for_dir(native_pdb_path, alphafold_output_dir)
-    print('Running lddt')
-    try:
-        global_lddt_df, local_lddt_dict = ModelAccuracy.get_lddt_for_dir(native_pdb_path, alphafold_output_dir)
-    except Exception as e:
-        print(e)
-        label_df = tmscore_df
+    if output_label_path.exists():
+        print('Accuracy has been already calculated.')
+        df = pd.read_csv(output_label_path, index_col=0)
     else:
-        label_df = pd.merge(tmscore_df, global_lddt_df, on='Model')
-        output_lddt_path = output_label_path.with_suffix('.lddt.npz')
-        np.savez_compressed(output_lddt_path, **local_lddt_dict)
-    score_df = pd.read_csv(alphafold_score_path, index_col=0)
-    df = pd.merge(label_df, score_df, on='Model').sort_values('GDT_TS').reset_index(drop=True)
-    df.to_csv(output_label_path)
+        print('Running TMscore')
+        tmscore_df = ModelAccuracy.get_gdt_for_dir(native_pdb_path, alphafold_output_dir)
+        print('Running lddt')
+        try:
+            global_lddt_df, local_lddt_dict = ModelAccuracy.get_lddt_for_dir(native_pdb_path, alphafold_output_dir)
+        except Exception as e:
+            print(e)
+            label_df = tmscore_df
+        else:
+            label_df = pd.merge(tmscore_df, global_lddt_df, on='Model')
+            output_lddt_path = output_label_path.with_suffix('.lddt.npz')
+            np.savez_compressed(output_lddt_path, **local_lddt_dict)
+        score_df = pd.read_csv(alphafold_score_path, index_col=0)
+        df = pd.merge(label_df, score_df, on='Model').sort_values('GDT_TS').reset_index(drop=True)
+        df['Num_diff'] = num_diff
+        df['Num_missing'] = num_missing
+        df['Length'] = length
+        df.to_csv(output_label_path)
 
     # Compress model_*.pickle
     tar_path = alphafold_output_dir / 'model_pickle.tar.gz'
