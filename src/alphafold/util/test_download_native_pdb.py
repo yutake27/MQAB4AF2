34b66550716144335a84d2e5578be2d1a071ecbe
yutake27 <takei.y.ah@m.titech.ac.jp>
2021-11-18 17:29:28 +0900

    feat: add test case for download pdb

--- src/alphafold/util/test_download_native_pdb.py
+++ src/alphafold/util/test_download_native_pdb.py
@@ -11,8 +11,13 @@ from download_native_pdb import DownloadProtein
 #     DownloadProtein.download_pdb_specific_chain(pdb_id, chain, fasta_seq, output_pdb_path)
 
 
+def make_fasta(header, sequence, fasta_file):
+    with open(fasta_file, 'w') as f:
+        f.write(f'{header}\n{sequence}\n')
+
+
 df = pd.read_csv('../../../data/interim/target_list.csv', index_col=0)
-num_test = 10
+num_test = 5
 
 
 @pytest.fixture(params=range(num_test))
@@ -21,11 +26,40 @@ def load_test_case(request, tmpdir):
     row = df.iloc[index]
     entry_id = row['id']
     pdb_id, chain = entry_id.split('_')
+    header = row['header']
     sequence = row['sequence']
-    tmpfile = tmpdir.join(pdb_id + '_' + chain + '.pdb')
-    yield (pdb_id, chain, sequence, tmpfile)
-    tmpfile.remove()
+    tmp_fastafile = tmpdir.join(f'{entry_id}.fasta')
+    make_fasta(header, sequence, tmp_fastafile)
+    tmp_pdbfile = tmpdir.join(f'{entry_id}.pdb')
+    yield (pdb_id, chain, tmp_fastafile, tmp_pdbfile)
+    tmp_pdbfile.remove()
 
 
 def test_download_protein(load_test_case):
     DownloadProtein.download_pdb_specific_chain(*load_test_case)
+
+
+missing_caces = ['7OF7_8', '5ZX9_A', '5QU5_A', '6HCZ_A', '7DCO_W']
+
+
+@pytest.fixture(params=missing_caces)
+def load_test_missing_case(request, tmpdir):
+    entry_id = request.param
+    pdb_id, chain = entry_id.split('_')
+    target_row = df.query('id == @entry_id').iloc[0]
+    header = target_row['header']
+    sequence = target_row['sequence']
+    print(header, sequence)
+    tmp_fastafile = tmpdir.join(f'{entry_id}.fasta')
+    make_fasta(header, sequence, tmp_fastafile)
+    tmp_pdbfile = tmpdir.join(f'{entry_id}.pdb')
+    yield (pdb_id, chain, tmp_fastafile, tmp_pdbfile)
+    tmp_pdbfile.remove()
+    tmp_fastafile.remove()
+
+
+def test_download_protein_missing(load_test_missing_case):
+    DownloadProtein.download_pdb_specific_chain(*load_test_missing_case)
+    _, _, tmp_fastafile, tmp_pdbfile = load_test_missing_case
+    num_diff, num_missing = DownloadProtein.test_match_pdb_and_fasta(tmp_pdbfile, tmp_fastafile)
+    assert num_diff < 10
